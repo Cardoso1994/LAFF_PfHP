21e775d8b82449f67d66346cd8408597f48c9d90
Marco Cardoso <marcoacardosom@gmail.com>
2020-10-05 23:53:17 -0500

    homework 1.3.6.2

--- Assignments/Week1/C/Gemm_J_Gemv.c
+++ Assignments/Week1/C/Gemm_J_Gemv.c
@@ -1,15 +1,18 @@
-#define alpha( i,j ) A[ (j)*ldA + i ]   // map alpha( i,j ) to array A 
-#define beta( i,j )  B[ (j)*ldB + i ]   // map beta( i,j )  to array B
-#define gamma( i,j ) C[ (j)*ldC + i ]   // map gamma( i,j ) to array C
+#define alpha(i, j) A[(j) * ldA + i]   // map alpha( i,j ) to array A 
+#define beta(i, j)  B[(j) * ldB + i]   // map beta( i,j )  to array B
+#define gamma(i, j) C[(j) * ldC + i]   // map gamma( i,j ) to array C
 
-void MyGemv( int, int, double *, int, double *, int, double *, int );
+void MyGemv(int, int, double *, int, double *, int, double *, int);
 
-void MyGemm( int m, int n, int k,
-	     double *A, int ldA,
-	     double *B, int ldB,
-	     double *C, int ldC )
+/* void MyGemv(int m, int n, double *A, int ldA,
+            double *x, int incx, double *y, int incy)
+*/
+
+void MyGemm(int m, int n, int k,
+			double *A, int ldA,
+			double *B, int ldB,
+			double *C, int ldC)
 {
-  for ( int j=0; j<n; j++ )
-    MyGemv(  ,  ,  ,  ,  ,  ,  ,   );
+	for (int j = 0; j < n; j++)
+		MyGemv(m, n, A, ldA, B + j * ldB, 1, C + j * ldC, 1);
 }
-  
