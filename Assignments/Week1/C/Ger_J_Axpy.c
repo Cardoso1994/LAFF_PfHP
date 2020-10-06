49a5137f74466671f45e40f418b132b9d4bda15a
Marco Cardoso <marcoacardosom@gmail.com>
2020-10-05 23:19:59 -0500

    homeworks 1.4.1.2 and 1.4.1.4

--- Assignments/Week1/C/Ger_J_Axpy.c
+++ Assignments/Week1/C/Ger_J_Axpy.c
@@ -1,12 +1,13 @@
-#define alpha( i,j ) A[ (j)*ldA + i ]   // map alpha( i,j ) to array A 
-#define chi( i )  x[ (i)*incx ]         // map chi( i )  to array x
-#define psi( i )  y[ (i)*incy ]         // map psi( i )  to array y
+#define alpha(i, j) A[(j) * ldA + i]   // map alpha( i,j ) to array A 
+#define chi(i)  x[(i) * incx]         // map chi( i )  to array x
+#define psi(i)  y[(i) * incy]         // map psi( i )  to array y
 
 void Axpy( int, double, double *, int, double *, int );
+void Axpy( int n, double alpha, double *x, int incx, double *y, int incy )
 
 void MyGer( int m, int n, double *x, int incx,
-	  double *y, int incy, double *A, int ldA )
+		double *y, int incy, double *A, int ldA )
 {
   for ( int j=0; j<n; j++ )
-    Axpy(    ,      ,     ,      ,       ,        );
+    Axpy(m, y + j * incy, x, incx, A + j * ldA, 1);
 }
